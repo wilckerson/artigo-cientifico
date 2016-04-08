ed0a75e30e7979d5dbd978654e95307dd837a202
wilckerson <wilckerson@gmail.com>
2016-04-08 09:04:24 -0300

    Criacao projeto nativo ios webRequestPerformance

---
+++ webRequestPerformance/native ios/WebRequestPerformance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebRequestPerformance
+//
+//  Created by Wilckerson Ganda on 08/04/16.
+//  Copyright © 2016 Wilckerson Ganda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
